43e3327af33b13daf3cae2c809bb92654fb5a8d1
aknambiar <aditya.k.nambiar@gmail.com>
2023-06-26 16:45:51 +0530

    Update specs to use expect instead of should

--- spec/about_array_assignment_spec.rb
+++ spec/about_array_assignment_spec.rb
@@ -1,46 +1,54 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-describe "Array Assignment" do
-
+describe 'Array Assignment' do
   it 'should demonstrate non-parallel assignment' do
-    names = ["John", "Smith"]
-    names.should eql __
+    names = ['John', 'Smith']
+
+    expect(names).to eql __
   end
 
   it 'should demonstrate parallel assignment' do
-    first_name, last_name = ["John", "Smith"]
-    first_name.should eql __
-    last_name.should eql __
+    first_name, last_name = ['John', 'Smith']
+
+    expect(first_name).to eql __
+    expect(last_name).to eql __
   end
 
   it 'should demonstrate parallel assignments with splat operator' do
-    first_name, *last_name = ["John", "Smith", "III"]
-    first_name.should eql __
-    last_name.should eql __
+    first_name, *last_name = ['John', 'Smith', 'III']
+
+    expect(first_name).to eql __
+    expect(last_name).to eql __
   end
 
   it 'should demonstrate parallel assignments with too few variables' do
-    first_name, last_name = ["Cher"]
-    first_name.should eql __
-    last_name.should eql __
+    first_name, last_name = ['Cher']
+
+    expect(first_name).to eql __
+    expect(last_name).to eql __
   end
 
   it 'should demonstrate parallel assignments with subarrays' do
-    first_name, last_name = [["Willie", "Rae"], "Johnson"]
-    first_name.should eql __
-    last_name.should eql __
+    first_name, last_name = [['Willie', 'Rae'], 'Johnson']
+
+    expect(first_name).to eql __
+    expect(last_name).to eql __
   end
 
   it 'should demonstrate parallel assignments with one variable' do
-    first_name, = ["John", "Smith"]
-    first_name.should eql __
+    first_name, = ['John', 'Smith']
+
+    expect(first_name).to eql __
   end
 
   it 'should demonstrate swapping with parallel assignment' do
-    first_name = "Roy"
-    last_name = "Rob"
+    first_name = 'Roy'
+    last_name = 'Rob'
     first_name, last_name = last_name, first_name
-    first_name.should eql __
-    last_name.should eql __
+
+    expect(first_name).to eql __
+    expect(last_name).to eql __
   end
 end
