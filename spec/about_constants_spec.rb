f6ad53ee099401bfa80a6272f96e325d8ba59457
aknambiar <aditya.k.nambiar@gmail.com>
2023-07-03 10:34:16 +0530

    Update to new expect syntax

--- spec/about_constants_spec.rb
+++ spec/about_constants_spec.rb
@@ -2,94 +2,92 @@ require 'spec_helper'
 
 C = 'top level'
 
-module AboutConstants
+class AboutConstants
+
   C = 'nested'
 
+  describe 'Constants' do
 
-describe "Constants" do
+    it 'should demonstrate nested_constants_may_also_be_referenced_with_relative_paths' do
+      expect(C).to eql __
+    end
 
-  # about_constants = AboutConstants.new
+    it 'should demonstrate top_level_constants_are_referenced_by_double_colons' do
+      expect(::C).to eql __
+    end
 
-  it "should demonstrate nested_constants_may_also_be_referenced_with_relative_paths" do
-    const = C
-    expect(const).to eql __
-  end
-
-  it "should demonstrate top_level_constants_are_referenced_by_double_colons" do
-    const = ::C
-    expect(const).to eql __
-  end
-
-  it "should demonstrate nested_constants_are_referenced_by_their_complete_path" do
-    AboutConstants::C.should eql __
-    ::AboutConstants::C.should eql __
+    it 'should demonstrate nested_constants_are_referenced_by_their_complete_path' do
+      expect(AboutConstants::C).to eql __
+      expect(::AboutConstants::C).to eql __
+    end
   end
 end
-end
+
+# ------------------------------------------------------------------
+
+describe 'Constants' do
+
+  class Animal
+    LEGS = 4
+    def legs_in_animal
+      LEGS
+    end
+
+    class NestedAnimal
+      def legs_in_nested_animal
+        LEGS
+      end
+    end
+  end
+
+  it 'should demonstrate nested_classes_inherit_constants_from_enclosing_classes' do
+    expect(Animal::NestedAnimal.new.legs_in_nested_animal).to eql __
+  end
 
   # ------------------------------------------------------------------
 
-#   class Animal
-#     LEGS = 4
-#     def legs_in_animal
-#       LEGS
-#     end
+  class Reptile < Animal
+    def legs_in_reptile
+      LEGS
+    end
+  end
 
-#     class NestedAnimal
-#       def legs_in_nested_animal
-#         LEGS
-#       end
-#     end
-#   end
+  it 'should demonstrate subclasses_inherit_constants_from_parent_classes' do
+    expect(Reptile.new.legs_in_reptile).to eql __
+  end
 
-#   it "should demonstrate nested_classes_inherit_constants_from_enclosing_classes" do
-#     Animal::NestedAnimal.new.legs_in_nested_animal.should eql __
-#   end
+  # ------------------------------------------------------------------
 
-#   # ------------------------------------------------------------------
+  class MyAnimals
+    LEGS = 2
 
-#   class Reptile < Animal
-#     def legs_in_reptile
-#       LEGS
-#     end
-#   end
+    class Bird < Animal
+      def legs_in_bird
+        LEGS
+      end
+    end
+  end
 
-#   it "should demonstrate subclasses_inherit_constants_from_parent_classes" do
-#     Reptile.new.legs_in_reptile.should eql __
-#   end
+  it 'should demonstrate who_wins_with_both_nested_and_inherited_constants' do
+    expect(MyAnimals::Bird.new.legs_in_bird).to eql __
+  end
 
-#   # ------------------------------------------------------------------
+  # QUESTION: Which has precedence: The constant in the lexical scope,
+  # or the constant from the inheritance hierarchy?
 
-#   class MyAnimals
-#     LEGS = 2
+  # ------------------------------------------------------------------
 
-#     class Bird < Animal
-#       def legs_in_bird
-#         LEGS
-#       end
-#     end
-#   end
+  class MyAnimals::Oyster < Animal
+    def legs_in_oyster
+      LEGS
+    end
+  end
 
-#   it "should demonstrate who_wins_with_both_nested_and_inherited_constants" do
-#     MyAnimals::Bird.new.legs_in_bird.should eql __
-#   end
+  it "should demonstrate who_wins_with_explicit_scoping_on_class_definition" do
+    MyAnimals::Oyster.new.legs_in_oyster.should eql __
+  end
 
-#   # QUESTION: Which has precedence: The constant in the lexical scope,
-#   # or the constant from the inheritance hierarchy?
-
-#   # ------------------------------------------------------------------
-
-#   class MyAnimals::Oyster < Animal
-#     def legs_in_oyster
-#       LEGS
-#     end
-#   end
-
-#   it "should demonstrate who_wins_with_explicit_scoping_on_class_definition" do
-#     MyAnimals::Oyster.new.legs_in_oyster.should eql __
-#   end
-
-#   # QUESTION: Now which has precedence: The constant in the lexical
-#   # scope, or the constant from the inheritance hierarchy?  Why is it
-#   # different than the previous answer?
-# end
+  # QUESTION: Now which has precedence: The constant in the lexical
+  # scope, or the constant from the inheritance hierarchy?  Why is it
+  # different than the previous answer?
+end
